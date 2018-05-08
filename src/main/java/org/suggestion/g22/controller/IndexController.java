673829aa2c9c3309f8f1b2cd3dbf375e6690e49e
nirmal <nirmal@nirmal-Aspire-A315-51>
2018-05-08 21:11:59 +0530

    Initialization Testing

---
+++ src/main/java/org/suggestion/g22/controller/IndexController.java
@@ -0,0 +1,14 @@
+package org.suggestion.g22.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController
+{
+	@RequestMapping("/index")
+	public String index()
+	{
+		return "/gui/index.jsp";
+	}
+}
