3f7e960148083745293a7ab2a779af0e1fa55aaf
nirmal <nirmal@nirmal-Aspire-A315-51>
2018-06-01 01:03:41 +0530

    Project Closed

--- src/main/java/org/web/controller/HelpController.java
+++ src/main/java/org/web/controller/HelpController.java
@@ -4,11 +4,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class HowToController
+public class HelpController
 {
-	@RequestMapping("/howto")
+	@RequestMapping("/help")
 	public String howTo()
 	{
-		return "howto";
+		return "help";
 	}
 }
